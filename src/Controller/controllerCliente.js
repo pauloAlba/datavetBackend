3f35cbe40594cea07476b9e6560a5cda02c98770
Paulo Alba <pr2013com@hotmail.com>
2023-02-07 19:35:21 -0300

    https adicionado

--- src/Controller/controllerCliente.js
+++ src/Controller/controllerCliente.js
@@ -1,4 +1,4 @@
-import { openDb } from "./configDB.js";
+import { openDb } from "../configDB.js";
 
 
 // INSERIR DADOS
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import router from "./router.js";
 import cors from "cors"
+import https from "https"
+import fs from "fs"
 
 const app = express();
 app.use(express.json());
@@ -10,5 +12,10 @@ app.use(router)
 const port = "443"
 
 app.set('port', process.env.PORT || 3000);
-app.listen(3000, ()=> console.log("Api Rodando."))
+
+
+https.createServer({
+  cert: fs.readFileSync("src/SSL/code.crt"),
+  key: fs.readFileSync("src/SSL/code.key")
+}, app).listen(port, () => console.log("Rodando em https"))
 
